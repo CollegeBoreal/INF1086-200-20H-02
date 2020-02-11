e652785e767dbc831a0497018031c73f5797ab12
AbbasSadissou <43480195+AbbasSadissou@users.noreply.github.com>
2020-02-11 10:34:17 -0500

    Update b300111671.py

--- 2.XDEVAPI/300111671/b300111671.py
+++ 2.XDEVAPI/300111671/b300111671.py
@@ -5,3 +5,25 @@ Created on Tue Jan 21 12:09:18 2020
 @author: LENOVO
 """
 
+# Connect to a Server
+mySession = mysqlx.get_session( {
+        'host': 'localhost', 'port': 33060,
+        'user': 'abbas', 'password': 'password'} )
+
+myDb = mySession.get_schema('world_x')
+
+# Create a new Collection
+myColl = myDb.create_collection('boubou')
+
+# Insert documents
+myColl.add({'_id': '1', 'COULEUR': 'noir', 'GENRE': 'dashiki', 'QUANTITE':'1 a 3').execute()
+myColl.add({'_id': '2', 'COULEUR': 'bleu', 'GENRE': 'alabe', 'QUANTITE': '1 douzaine').execute()
+
+# Find a document
+docs = myColl.find('COULEUR like: param1 AND GENRE < : param2').limit(1).bind('param1', 'I%').bind('param2', 'V%').execute()
+
+# Print document
+doc = docs.fetch_one()
+print(doc)
+
+myDb.drop_collection('boubou')

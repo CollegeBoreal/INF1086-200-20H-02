5c4a7fc82fe4781a3d34053891f3e1b713c169c4
halimabzn <halimafd79@gmail.com>
2020-02-11 10:53:52 -0500

    .

--- 2.XDEVAPI/300108234/b300108234.py
+++ 2.XDEVAPI/300108234/b300108234.py
@@ -2,7 +2,58 @@
 """
 Created on Tue Jan 21 10:45:16 2020
 
-@author: User
+@author: halima
 """
 
-print ("سلام")
+# Connecting to MySQL Server and working with a Collection
+from mysqlsh import mysqlx
+
+# Connect to server
+def connect():
+  return mysqlx.get_session( { 'host': 'localhost', 'port': 33060, 'user': 'halima', 'password': 'password'} )
+
+# Connect to DB
+def dbConnect(session, myDb):
+  return session.get_schema(myDb)
+
+# Create a new collection
+def dbCreateCollection(myDb, myCollection):
+  return myDb.create_collection(myCollection)
+
+# Insert documents
+def dbCRUD(myColl):
+  myColl.add({'_id': '1', 'name': 'Canada', 'code_postal': 1234}).execute()
+  myColl.add({'_id': '2', 'name': 'USA', 'code_postal': 2345}).execute()
+  myColl.add({'_id': '3', 'name': 'France', 'code_postal': 6789}).execute()
+
+# Find a document
+def dbFind(myColl):
+  return myColl.find('_id like :param').limit(1).bind('param','C%') \
+            .limit(1) \
+            .bind('param','C%') \
+            .execute()
+
+# Drop the collection
+def dbDropCollection(myDb, myCollection):
+  return myDb.drop_collection(myCollection)
+
+# Program
+def run(myDb, myCollection):
+  session = connect()
+  db = dbConnect(session, myDb)
+  col = dbCreateCollection(db, myCollection)
+  dbCRUD(col)
+  docs = dbFind(col)
+  # Print document
+  doc = docs.fetch_one()
+  print(doc)
+  dbDropCollection(db, myCollection)
+
+# Program
+def main():
+  myDb = 'world_x'
+  myCollection = 'my_collection'
+  run(myDb, myCollection)
+
+if __name__== "__main__":
+    main()

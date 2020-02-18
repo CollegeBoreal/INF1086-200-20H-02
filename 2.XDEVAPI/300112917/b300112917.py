bb95539ce8bbe7a330be951f35e4e45cd8790f57
djumaster <djumaster@gmail.com>
2020-02-18 17:27:04 -0500

    Djumaster

--- 2.XDEVAPI/300112917/b300112917.py
+++ 2.XDEVAPI/300112917/b300112917.py
@@ -4,4 +4,34 @@ Created on Tue Jan 21 10:44:59 2020
 
 @author: djumaster
 """
+# Connecting to MySQL Server and working with a Collection
+from mysqlsh import mysqlx
 
+# Connect to server
+mySession = mysqlx.get_session( {
+'host': 'localhost', 'port': 33060,
+'user': 'etudiants', 'password': 'etudiants_1'} )
+
+myDb = mySession.get_schema('world_x')
+
+# Create a new collection 'my_collection'
+myColl = myDb.create_collection('employee')
+
+# Insert documents
+myColl.add({'_id': '1', 'name': 'Laurie', 'age': 19}).execute()
+myColl.add({'_id': '2', 'name': 'Nadya', 'age': 54}).execute()
+myColl.add({'_id': '3', 'name': 'Lukas', 'age': 32}).execute()
+
+# Find a document
+docs = myColl.find('name like :param1 AND age < :param2') \
+          .limit(1) \
+          .bind('param1','L%') \
+          .bind('param2',20) \
+          .execute()
+
+# Print document
+doc = docs.fetch_one()
+print(doc)
+
+# Drop the collection
+myDb.drop_collection('my_collection')

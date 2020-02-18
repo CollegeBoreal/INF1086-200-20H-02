ba488f53bf48beb49ab6cfed498b54b3405e8fbd
setrar <setrar@valiha.com>
2020-02-17 23:32:19 -0500

    Update b300098957.py

--- 4.SQL-NoSQL/300098957/b300098957.py
+++ 4.SQL-NoSQL/300098957/b300098957.py
@@ -8,6 +8,23 @@ Created on 2020
 # Connecting to MySQL Server and working with a Collection
 from mysqlsh import mysqlx
 
+
+def mashup(myDb):
+   # Create a new collection 'my_collection'
+   myColl = myDb.get_collection('import_students')
+
+   # Accessing an existing table
+   myTable = myDb.get_table('ETUDIANTS')
+
+   # Find objects from document
+   objs = myColl.find().execute()
+
+   for obj in objs.fetch_all():
+      for student in obj.students.student:
+         print("INSERT INTO ETUDIANTS (initiale,nom) VALUES ('"+student.name.first_name+"', '"+ student.name.last_name+"')")
+         # Insert SQL Table data
+         myTable.insert(['initiale','nom']).values(student.name.first_name, student.name.last_name).execute()
+
 # Connect to server
 mySession = mysqlx.get_session( {
 'host': 'localhost', 'port': 33060,
@@ -15,19 +32,7 @@ mySession = mysqlx.get_session( {
 
 myDb = mySession.get_schema('etudiants')
 
-# Create a new collection 'my_collection'
-myColl = myDb.get_collection('import_students')
+mashup(myDb)
 
-# Accessing an existing table
-myTable = myDb.get_table('ETUDIANTS')
-
-# Find objects from document
-objs = myColl.find().execute()
-
-for obj in objs.fetch_all():
-   for student in obj.students.student:
-      print("INSERT INTO ETUDIANTS (initiale,nom) VALUES ('"+student.name.first_name+"', '"+ student.name.last_name+"')")
-      # Insert SQL Table data
-      myTable.insert(['initiale','nom']).values(student.name.first_name, student.name.last_name).execute()
 
 

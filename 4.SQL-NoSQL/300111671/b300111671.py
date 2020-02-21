1e266f87891ecce66d79678c6a7f5f51353c54f6
AbbasSadissou <aladeabbas@gmail.com>
2020-02-21 18:08:47 -0500

    kk

--- 4.SQL-NoSQL/300111671/b300111671.py
+++ 4.SQL-NoSQL/300111671/b300111671.py
@@ -7,6 +7,16 @@ Created on Wed Feb 19 18:43:39 2020
 
 from mysqlsh import mysqlx 
 
+
+# La fonction mashup
+
+def mashup(myDb):
+   # Create a new collection 'my_collection'
+myColl = myDb.get_collection('import_Boubou')
+
+
+
+
 # Connect to a Server
 mySession = mysqlx.get_session( {
         'host': 'localhost', 'port': 33060,
@@ -27,5 +37,11 @@ docs = myColl.find('COULEUR like: param').limit(1).bind('param', 'noir').execute
 # Print document
 doc = docs.fetch_one()
 print(doc)
+
+# La fonction mashup
+
+def mashup(myDb):
+   # Create a new collection 'my_collection'
+myColl = myDb.get_collection('import_Boubou')
 
 myDb.drop_collection('Boubou2')

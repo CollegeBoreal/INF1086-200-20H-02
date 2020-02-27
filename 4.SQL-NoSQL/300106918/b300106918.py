78f241068f44e2f7a3d1a03753a30bf88243ca61
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2020-02-27 01:05:59 -0500

    Update b300106918.py

--- 4.SQL-NoSQL/300106918/b300106918.py
+++ 4.SQL-NoSQL/300106918/b300106918.py
@@ -5,4 +5,33 @@ Created on Tue Jan 21 10:45:03 2020
 @author: acer
 """
 
-print("Bonjour le monde")
+# -*- coding: utf-8 -*-
+
+"""
+
+# Connecting to MySQL Server and working with a Collection
+
+from mysqlsh import mysqlx
+# La fonction mashup
+
+def mashup(myDb):
+   # Create a new collection 'aek_collection'
+   myColl = myDb.get_collection('import_cisco')
+   # Accessing an existing table
+   myTable = myDb.get_table('network_IT')
+   
+   # Find objects from document
+   objs = myColl.find().execute()
+   for obj in objs.fetch_all():
+      for product_list in obj.pagination_response_record.product_list:
+         print("INSERT INTO network_IT (Nom_produit, date) VALUES ('"+product_list.type.product_type+"', '"+ product_list.date.release_date+"')")
+         # Insert SQL Table data
+         myTable.insert(['Nom_produit','date']).values(product_list.type.product_type, product_list.date.release_date).execute()
+         
+# Connect to server
+mySession = mysqlx.get_session( {
+'host': 'localhost', 'port': 33060,
+'user': 'AEK', 'password': 'etudiants_1'} )
+myDb = mySession.get_schema('network_IT')
+mashup(myDb)
+

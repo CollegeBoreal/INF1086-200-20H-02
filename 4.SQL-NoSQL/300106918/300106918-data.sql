9a5a98d6f85c1033316ac8a6083f431a30e99e4c
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2020-02-26 22:38:38 -0500

    Update 300106918-data.sql

--- 4.SQL-NoSQL/300106918/300106918-data.sql
+++ 4.SQL-NoSQL/300106918/300106918-data.sql
@@ -6,24 +6,19 @@ use network_IT;
 -- Données de la table SOLUTIONS
 
 
-INSERT INTO SOLUTIONS ( solution, Nom_solution, date ) VALUES (22, 'configuration', '2020-01-01');
-INSERT INTO SOLUTIONS ( solution, Nom_solution, date  ) VALUES (34, 'vlan', '2019-01-10');
-INSERT INTO SOLUTIONS ( solution, Nom_solution, date  ) VALUES (56, 'routeur', '2020-12-12');
+INSERT INTO SOLUTIONS ( solution, Nom_solution, date ) VALUES (21, 'configuration', '2020-01-01');
+INSERT INTO SOLUTIONS ( solution, Nom_solution, date  ) VALUES (31, 'vlan', '2019-01-10');
+INSERT INTO SOLUTIONS ( solution, Nom_solution, date  ) VALUES (51, 'routeur', '2020-12-12');
 
 -- Données de la table PRODUITS
 
 INSERT INTO PRODUCTS ( IDproduit, Nom_produit, date, total_en_stock ) VALUES (10, 'router', '2020-11-09', 233);
 INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock ) VALUES (20, 'switches','2014-10-04', 130);
 INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock) VALUES (30, 'camera', '2014-10-01', 144);
-INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock) VALUES (40, 'camera', '2014-10-01', 150);
-INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock) VALUES (50, 'camera', '2014-10-01', 166);
-INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock) VALUES (60, 'camera', '2014-10-01', 6);
+
 
 -- Données de la table CLIENTS
 
-INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (40, 'olitan', 'toronto');
-INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (50, 'aek', 'northyork');
-INSERT INTO CLIENTS (IDclient, Nom_client, ville) VALUES (60, 'jemma', 'mississauga');
 INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (70, 'check', 'toronto');
 INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (80, 'abbas', 'toronto');
 INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (90, 'amir', 'sudbury');
@@ -31,9 +26,9 @@ INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (90, 'amir', 'sudbury'
 
 -- Données de la table insttalation
 
-INSERT INTO INSTALLATIONS (installation, Nom_installation  ) VALUES (4, 'firewall');
-INSERT INTO INSTALLATIONS (installation, Nom_installation ) VALUES (5, 'anti-virus');
-INSERT INTO INSTALLATIONS (installation, Nom_installation ) VALUES (6, 'mise a jours');
+INSERT INTO INSTALLATIONS (installation, Nom_installation  ) VALUES (7, 'firewall');
+INSERT INTO INSTALLATIONS (installation, Nom_installation ) VALUES (8, 'anti-virus');
+INSERT INTO INSTALLATIONS (installation, Nom_installation ) VALUES (9, 'mise a jours');
                                                   
   -- Données de la table prix
 INSERT INTO PRIX (solution, installation, IDclient, prix ) VALUES (22, 4, 40, 1000);

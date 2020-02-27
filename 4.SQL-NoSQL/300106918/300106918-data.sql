de51e797aabf0b1b697159f06e85d52af075a19a
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2020-02-26 22:40:41 -0500

    Update 300106918-data.sql

--- 4.SQL-NoSQL/300106918/300106918-data.sql
+++ 4.SQL-NoSQL/300106918/300106918-data.sql
@@ -12,16 +12,16 @@ INSERT INTO SOLUTIONS ( solution, Nom_solution, date  ) VALUES (51, 'routeur', '
 
 -- Données de la table PRODUITS
 
-INSERT INTO PRODUCTS ( IDproduit, Nom_produit, date, total_en_stock ) VALUES (10, 'router', '2020-11-09', 233);
-INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock ) VALUES (20, 'switches','2014-10-04', 130);
-INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock) VALUES (30, 'camera', '2014-10-01', 144);
+INSERT INTO PRODUCTS ( IDproduit, Nom_produit, date, total_en_stock ) VALUES (11, 'router', '2020-11-09', 233);
+INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock ) VALUES (22, 'switches','2014-10-04', 130);
+INSERT INTO PRODUCTS ( IDproduit, Nom_produit, Date, total_en_stock) VALUES (33, 'camera', '2014-10-01', 144);
 
 
 -- Données de la table CLIENTS
 
-INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (70, 'check', 'toronto');
-INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (80, 'abbas', 'toronto');
-INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (90, 'amir', 'sudbury');
+INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (71, 'check', 'toronto');
+INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (81, 'abbas', 'toronto');
+INSERT INTO CLIENTS ( IDclient, Nom_client, ville) VALUES (91, 'amir', 'sudbury');
 
 
 -- Données de la table insttalation

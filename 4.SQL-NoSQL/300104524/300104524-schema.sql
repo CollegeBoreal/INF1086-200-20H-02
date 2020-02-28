e7fe59a7940f7d38d2d89c9c18573b508a68b9e7
Echnaideurgeneus <43045076+Echnaideurgeneus@users.noreply.github.com>
2020-02-28 14:16:50 -0500

    Update 300104524-schema.sql

--- 4.SQL-NoSQL/300104524/300104524-schema.sql
+++ 4.SQL-NoSQL/300104524/300104524-schema.sql
@@ -1,60 +1,115 @@
-CREATE DATABASE IF NOT EXISTS technical_support;
+-- MySQL Workbench Forward Engineering
 
-CREATE USER IF NOT EXISTS 'technical_support'@'localhost' IDENTIFIED BY 'technical_support_1';
-GRANT ALL ON technical_support.* TO 'technical_support'@'localhost';
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-use technical_support;
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema technical_support
+-- -----------------------------------------------------
 
-CREATE TABLE CLIENTS(
-	client INT NOT NULL AUTO_INCREMENT,
-		     nom VARCHAR(30),
-		    prenom VARCHAR(20),
-	PRIMARY KEY(client)
-);
+-- -----------------------------------------------------
+-- Schema technical_support
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `technical_support` DEFAULT CHARACTER SET latin1 ;
+USE `technical_support` ;
 
-CREATE TABLE SERVICES(
-service INT AUTO_INCREMENT,
-	client INT,
-BRAND VARCHAR(30) NOT NULL,
- problem VARCHAR(30) NOT NULL,
-description VARCHAR(120),
-	PRIMARY KEY(service),
-FOREIGN KEY(client)
-REFERENCES CLIENTS(client)
-);
+-- -----------------------------------------------------
+-- Table `technical_support`.`CLIENTS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `technical_support`.`CLIENTS` (
+  `client` INT(11) NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(30) NULL DEFAULT NULL,
+  `prenom` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`client`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
-CREATE TABLE REPAIRS(
-repair INT NOT NULL AUTO_INCREMENT,
-mac_adress VARCHAR(30),
- brand VARCHAR(30) NOT NULL,
-PRIMARY KEY(repair)
-);
 
-CREATE TABLE NOTES(
-archivre_num INT NOT NULL AUTO_INCREMENT,
-demarche VARCHAR(120)NOT NULL,
-documentation VARCHAR(120) NOT NULL,
-PRIMARY KEY(archivre_num)
-);
+-- -----------------------------------------------------
+-- Table `technical_support`.`NOTES`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `technical_support`.`NOTES` (
+  `archivre_num` INT(11) NOT NULL AUTO_INCREMENT,
+  `demarche` VARCHAR(120) NOT NULL,
+  `documentation` VARCHAR(120) NOT NULL,
+  PRIMARY KEY (`archivre_num`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
-CREATE TABLE PAYMENTS(
-commande_num INT NOT NULL AUTO_INCREMENT,
-article VARCHAR(30) NOT NULL,
-quantity INT(10),
- note INT,
-PRIMARY KEY(commande_num)
-);
- 
- 
-    CREATE TABLE VENTES(
-      num_vente INT NOT NULL, 
-      date_vente DATE,
-      article INT,
-      service INT,
-      client INT,
-PRIMARY KEY(client, service),
-       FOREIGN KEY (client)
-     REFERENCES CLIENTS (client),
-     FOREIGN KEY (service)
-    REFERENCES SERVICES(service)
-);
+
+-- -----------------------------------------------------
+-- Table `technical_support`.`SERVICES`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `technical_support`.`SERVICES` (
+  `service` INT(11) NOT NULL AUTO_INCREMENT,
+  `BRAND` VARCHAR(30) NOT NULL,
+  `problem` VARCHAR(30) NOT NULL,
+  `description` VARCHAR(120) NULL DEFAULT NULL,
+  PRIMARY KEY (`service`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `technical_support`.`VENTES`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `technical_support`.`VENTES` (
+  `vente` INT(11) NOT NULL AUTO_INCREMENT,
+  `date_vente` DATE NULL DEFAULT NULL,
+  `article` VARCHAR(120) NULL DEFAULT NULL,
+  `service` INT(11) NULL DEFAULT NULL,
+  `client` INT(11) NOT NULL,
+  PRIMARY KEY (`vente`, `client`),
+  INDEX `client` (`client` ) ,
+  INDEX `service` (`service` ) ,
+  CONSTRAINT `VENTES_ibfk_1`
+    FOREIGN KEY (`client`)
+    REFERENCES `technical_support`.`CLIENTS` (`client`),
+  CONSTRAINT `VENTES_ibfk_2`
+    FOREIGN KEY (`service`)
+    REFERENCES `technical_support`.`SERVICES` (`service`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `technical_support`.`PAYMENTS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `technical_support`.`PAYMENTS` (
+  `payment` INT(11) NOT NULL AUTO_INCREMENT,
+  `article` VARCHAR(30) NOT NULL,
+  `quantity` INT(10) NULL DEFAULT NULL,
+  `vente` INT(11) NOT NULL,
+  PRIMARY KEY (`payment`, `vente`),
+  INDEX `vente` (`vente` ) ,
+  CONSTRAINT `PAYMENTS_ibfk_1`
+    FOREIGN KEY (`vente`)
+    REFERENCES `technical_support`.`VENTES` (`vente`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `technical_support`.`REPAIRS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `technical_support`.`REPAIRS` (
+  `repair` INT(11) NOT NULL AUTO_INCREMENT,
+  `service` INT(11) NULL DEFAULT NULL,
+  `mac_adress` VARCHAR(30) NULL DEFAULT NULL,
+  `brand` VARCHAR(30) NOT NULL,
+  PRIMARY KEY (`repair`),
+  INDEX `service` (`service` ) ,
+  CONSTRAINT `REPAIRS_ibfk_1`
+    FOREIGN KEY (`service`)
+    REFERENCES `technical_support`.`SERVICES` (`service`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;

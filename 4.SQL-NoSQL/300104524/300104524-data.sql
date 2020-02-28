93bf8ca3136e633590e89e6c550a9fdc9b06682c
Echnaideurgeneus <43045076+Echnaideurgeneus@users.noreply.github.com>
2020-02-28 14:15:56 -0500

    Update 300104524-data.sql

--- 4.SQL-NoSQL/300104524/300104524-data.sql
+++ 4.SQL-NoSQL/300104524/300104524-data.sql
@@ -1,12 +1,18 @@
-
 INSERT INTO CLIENTS (nom, prenom) VALUES ('Jean', 'William');
 INSERT INTO CLIENTS (nom, prenom) VALUES ('Medgine', 'Pierre');
 INSERT INTO CLIENTS (nom, prenom) VALUES ('Jacques', 'Gerome');
 
-INSERT INTO SERVICE (services) VALUES ('laptop HP', 'start', 'Intel core 5g');
-INSERT INTO SERVICE (services) VALUES ('DELL', 'slow');
-INSERT INTO SERVICE (services) VALUES ('laptop HP', 'slow', 'Intel core 5g');
+INSERT INTO SERVICES (BRAND, problem, description) VALUES ('HP', 'hard drive', 'Not Bootable');
+INSERT INTO SERVICES (BRAND, problem, description) VALUES ('DELL', 'cpu', 'Heating');
 
-INSERT INTO SERVICE (reparation) VALUES ('hard drive', ' 00:00:00:a1:2b:cc', 'HP envy');
-INSERT INTO SERVICE (reparation) VALUES ('cpu', ' 00:00:00:a1:2b:cc', 'Dell 2010');
-INSERT INTO SERVICE (reparation) VALUES ('wireless card', ' 00:00:00:a1:2b:cc', 'hp 2015');
+INSERT INTO REPAIRS (service, mac_adress, brand) VALUES (1, '10:25:69:69', 'HP');
+INSERT INTO REPAIRS (service, mac_adress, brand) VALUES (2, '56:98:75:54', 'DELL');
+
+INSERT INTO NOTES (demarche, documentation) VALUES ('Test 1 a 1', 'HP SUPPORT');
+INSERT INTO NOTES (demarche, documentation) VALUES ('Installer une pate thermique', 'INTEL PROCESSOR');
+
+INSERT INTO VENTES (date_vente, article, service, client) VALUES ('2019-12-06', 'Reparation Immediat LAPTOP', 1, 1);
+INSERT INTO VENTES (date_vente, article, service, client) VALUES ('2019-12-09', 'Reparation sur Rendez-Vous DESKTOP', 2, 2);
+
+INSERT INTO PAYMENTS (article, quantity, vente) VALUES ('LAPTOP', 1, 1);
+INSERT INTO PAYMENTS (article, quantity, vente) VALUES ('DESKTOP', 1, 2);

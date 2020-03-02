2ee5a35404b09b4adc6ee5dd90fb2b3983d7cc89
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2020-03-02 04:40:34 -0500

    Update b300098957.py

--- 4.SQL-NoSQL/300098957/b300098957.py
+++ 4.SQL-NoSQL/300098957/b300098957.py
@@ -6,33 +6,29 @@ Created on 2020
 """
 
 # Connecting to MySQL Server and working with a Collection
-from mysqlsh import mysqlx
+import mysqlx
 
-# La fonction mashup
+
+
 def mashup(myDb):
-   # Create a new collection 'my_collection'
-   myColl = myDb.get_collection('import_students')
 
-   # Accessing an existing table
-   myTable = myDb.get_table('ETUDIANTS')
+    myColl = myDb.get_collection('cisco')
 
-   # Find objects from document
-   objs = myColl.find().execute()
+    myTable = myDb.get_table('network_IT')
 
-   for obj in objs.fetch_all():
-      for student in obj.students.student:
-         print("INSERT INTO ETUDIANTS (initiale,nom) VALUES ('"+student.name.first_name+"', '"+ student.name.last_name+"')")
+    objs = myColl.find().execute()
+
+    for obj in objs.fetch_all():
+
+      for product_list in obj.cisco.product_list:
+
+         print("INSERT INTO network_IT (Nom_produit, date) VALUES ('"+product_list.type.product_type+"', '"+ product_list.date.release_date+"')")
+
          # Insert SQL Table data
-         myTable.insert(['initiale','nom']).values(student.name.first_name, student.name.last_name).execute()
 
-# Connect to server
-mySession = mysqlx.get_session( {
-'host': 'localhost', 'port': 33060,
-'user': 'etudiants', 'password': 'etudiants_1'} )
+         myTable.insert(['Nom_produit','date']).values(product_list.type.product_type, product_list.date.release_date).execute()
 
-myDb = mySession.get_schema('etudiants')
-
-mashup(myDb)
+         mashup(myDb)
 
 
 

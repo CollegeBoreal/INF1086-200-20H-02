3707dc35c09c75189dce38fe3189baddb5b2b379
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2020-03-02 15:11:18 -0500

    Update b300106918.py

--- 4.SQL-NoSQL/300106918/b300106918.py
+++ 4.SQL-NoSQL/300106918/b300106918.py
@@ -14,24 +14,30 @@ Created on Tue Jan 21 10:45:03 2020
 from mysqlsh import mysqlx
 # La fonction mashup
 
-def mashup(myDb):
-   # Create a new collection 'my_collection'
-   myColl = myDb.get_collection('cisco')
-   # Accessing an existing table
-   myTable = myDb.get_table('network_IT')
-   
-   # Find objects from document
-   objs = myColl.find().execute()
-   for obj in objs.fetch_all():
-      for product_list in obj.cisco.product_list:
-         print("INSERT INTO network_IT (Nom_produit, date) VALUES ('"+product_list.type.product_type+"', '"+ product_list.date.release_date+"')")
-         # Insert SQL Table data
-         myTable.insert(['Nom_produit','date']).values(product_list.type.product_type, product_list.date.release_date).execute()
-         
-# Connect to server
-mySession = mysqlx.get_session( {
-'host': 'localhost', 'port': 33060,
-'user': 'AEK', 'password': 'etudiants_1'} )
-myDb = mySession.get_schema('network_IT')
-mashup(myDb)
+from mysqlsh import mysqlx
 
+def mashup(myDb):
+
+    myColl = myDb.get_collection('cisco')
+
+    myTable = myDb.get_table('network_IT')
+
+    objs = myColl.find().execute()
+
+    for obj in objs.fetch_all():
+
+       for product_list in obj.cisco.product_list:
+
+         print("INSERT INTO network_IT (Nom_produit, date) VALUES ('"+product_list.type.product_type+"', '"+ product_list.date.release_date+"')")
+
+         # Insert SQL Table data
+
+         myTable.insert(['Nom_produit','date']).values(product_list.type.product_type, product_list.date.release_date).execute()
+
+
+mySession = mysqlx.get_session( {
+
+'host': 'localhost', 'port': 33060,
+
+'user': 'AEK', 'password': 'etudiants_1'} )
+mashup(myDb)

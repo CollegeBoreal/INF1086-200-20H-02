f0429e09219ecc52be6140bed8c718ad774b14a0
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2020-03-02 15:36:58 -0500

    Update b300106918.py

--- 4.SQL-NoSQL/300106918/b300106918.py
+++ 4.SQL-NoSQL/300106918/b300106918.py
@@ -18,22 +18,3 @@ mydb = mysql.connector.connect(
   passwd="etudiants_1",
   database="network_IT"
 )
-def mashup(myDb):
-
-    myColl = myDb.get_collection('cisco')
-
-    myTable = myDb.get_table('network_IT')
-
-    objs = myColl.find().execute()
-
-    for obj in objs.fetch_all():
-
-      for product_list in obj.cisco.product_list:
-
-         print("INSERT INTO network_IT (Nom_produit, date) VALUES ('"+product_list.type.product_type+"', '"+ product_list.date.release_date+"')")
-
-         # Insert SQL Table data
-
-         myTable.insert(['Nom_produit','date']).values(product_list.type.product_type, product_list.date.release_date).execute()
-
-         mashup(myDb)

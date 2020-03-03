6d6f59f8168fc933f188214be50c7b8730d69c99
ordenrosae <43898171+ordenrosae@users.noreply.github.com>
2020-03-03 17:31:10 -0500

    Update b300112017.py

--- 4.SQL-NoSQL/300112017/b300112017.py
+++ 4.SQL-NoSQL/300112017/b300112017.py
@@ -5,38 +5,38 @@ Created on Sat Feb 29 19:58:50 2020
 @author: Ador
 """
 
-	# Connecting to MySQL Server and working with a Collection
-	from mysqlsh import mysqlx
-	
+# Connecting to MySQL Server and working with a Collection
+from mysqlsh import mysqlx
 
-	# La fonction mashup
-	def mashup(myDb):
-	   # Create a new collection 'my_collection'
-	   myColl = myDb.get_collection('Moto')
-	
 
-	   # Accessing an existing table
-	   myTable = myDb.get_table('MODELS')
-	
+# La fonction mashup
+def mashup(myDb):
+# Create a new collection 'my_collection'
+myColl = myDb.get_collection('Moto')
 
-	   # Find objects from document
-	   objs = myColl.find().execute()
-	
 
-	   for obj in objs.fetch_all():
-	      for name in obj.data.name:
-	         print("INSERT INTO MODELS (Models) VALUES ('"+data.name+"')")
-	         # Insert SQL Table data
-	         myTable.insert(['Models']).values(data.name).execute()
-	
+# Accessing an existing table
+myTable = myDb.get_table('MODELS')
 
-	# Connect to server
-	mySession = mysqlx.get_session( {
-	'host': 'localhost', 'port': 33060,
-	'user': 'ador', 'password': 'password'} )
-	
 
-	myDb = mySession.get_schema('magasin')
-	
+# Find objects from document
+objs = myColl.find().execute()
 
-	mashup(myDb)
+
+for obj in objs.fetch_all():
+for name in obj.data.name:
+print("INSERT INTO MODELS (Models) VALUES ('"+data.name+"')")
+# Insert SQL Table data
+myTable.insert(['Models']).values(data.name).execute()
+
+
+# Connect to server
+mySession = mysqlx.get_session( {
+'host': 'localhost', 'port': 33060,
+'user': 'ador', 'password': 'password'} )
+
+
+myDb = mySession.get_schema('magasin')
+
+
+mashup(myDb)

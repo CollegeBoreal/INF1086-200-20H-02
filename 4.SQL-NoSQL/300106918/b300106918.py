984a40116c4b52d4c38d6fc7d0f70ecb971bc8ff
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2020-03-03 01:45:59 -0500

    Update b300106918.py

--- 4.SQL-NoSQL/300106918/b300106918.py
+++ 4.SQL-NoSQL/300106918/b300106918.py
@@ -10,11 +10,33 @@ Created on Tue Jan 21 10:45:03 2020
 
 """
 
+import mysqlx
 
-import mysql.connector
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="AEK",
-  passwd="etudiants_1",
-  database="network_IT"
-)
+def mashup(myDb):
+
+    myColl = myDb.get_collection('cisco')
+
+    myTable = myDb.get_table('network_IT')
+
+    objs = myColl.find().execute()
+
+    for obj in objs.fetch_all():
+
+       for product_list in obj.cisco.product_list:
+
+         print("INSERT INTO network_IT (Nom_produit, date) VALUES ('"+product_list.type.product_type+"', '"+ product_list.date.release_date+"')")
+
+         # Insert SQL Table data
+
+         myTable.insert(['Nom_produit','date']).values(product_list.type.product_type, product_list.date.release_date).execute()
+
+
+mySession = mysqlx.get_session( {
+
+'host': 'localhost', 'port': 33060,
+
+'user': 'AEK', 'password': 'etudiants_1'} )
+
+myDb = mySession.get_schema('network_IT')
+
+mashup(myDb)

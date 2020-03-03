ebad26e4ba37cc0d5a50f7c7280658236ca7d3ff
8dbe <43479739+8dbe@users.noreply.github.com>
2020-03-03 15:13:19 -0500

    Update b300113775.py

--- 4.SQL-NoSQL/300113775/b300113775.py
+++ 4.SQL-NoSQL/300113775/b300113775.py
@@ -1,1 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2020
+@author: 300113775
+"""
 
+# Connecting to MySQL Server and working with a Collection
+from mysqlsh import mysqlx
+
+# La fonction mashup
+def mashup(myDb):
+   # Create a new collection 'my_collection'
+   myColl = myDb.get_collection('photographie')
+
+   # Accessing an existing table
+   myTable = myDb.get_table('IMAGES')
+
+   # Find objects from document
+   objs = myColl.find().execute()
+
+   for obj in objs.fetch_all():
+      for url in obj.properties.url:
+         print("INSERT INTO IMAGES (Images) VALUES ('"+url.format+"')")
+         # Insert SQL Table data
+         myTable.insert(['Images']).values(url.format).execute()
+
+# Connect to server
+mySession = mysqlx.get_session( {
+'host': 'localhost', 'port': 33060,
+'user': 'widby', 'password': 'Password123'} )
+
+myDb = mySession.get_schema('widesign')
+
+mashup(myDb)

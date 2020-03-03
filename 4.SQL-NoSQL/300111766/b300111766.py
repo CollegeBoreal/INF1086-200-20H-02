f674cd60e7ef8ee751bfc03b2fdb95ef236b6a2c
cheikthiam <44683625+Cheikhthiam@users.noreply.github.com>
2020-03-03 14:25:21 -0800

    .

--- 4.SQL-NoSQL/300111766/b300111766.py
+++ 4.SQL-NoSQL/300111766/b300111766.py
@@ -14,19 +14,17 @@ Created on 2020
 	   myColl = myDb.get_collection('import_phone')
 	
 
-	   # Accessing an existing table
-	   myTable = myDb.get_table('MARK')
 # Accessing an existing table
-   myTable = myDb.get_table('MARK')
+   myTable = myDb.get_table('MARKS')
 
    # Find objects from document
    objs = myColl.find().execute()
 
    for obj in objs.fetch_one():
-      for MARK in obj.MARK:
-         print("INSERT INTO MARK(mark, name) VALUES ('"+mark+"', '"+name+"')")
+      for name in obj.name:
+         print("INSERT INTO MARKS(name) VALUES ('"+name+"')")
          # Insert SQL Table data
-         myTable.insert(['mark','name']).values(MARK.mark, MARK.name).execute()
+         myTable.insert(['name']).values(name).execute()
 
 # Connect to server
 mySession = mysqlx.get_session( {

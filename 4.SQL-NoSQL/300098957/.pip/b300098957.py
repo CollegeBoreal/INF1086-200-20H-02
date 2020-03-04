31a0878af2aac13ebe7556063aa61afe9f52317e
setrar <setrar@valiha.com>
2020-03-03 19:15:11 -0500

    .

--- 4.SQL-NoSQL/300098957/.pip/b300098957.py
+++ 4.SQL-NoSQL/300098957/.pip/b300098957.py
@@ -8,16 +8,16 @@ session = mysqlx.get_session({
     'password': 'etudiants_1'
 })
 
-# schema = session.get_schema('test')
+schema = session.get_schema('etudiants')
 
 # Use the collection 'my_collection'
-# collection = schema.get_collection('my_collection')
+collection = schema.get_collection('import_programs')
 
 # Specify which document to find with Collection.find()
-# result = collection.find('name like :param').bind('param', 'S%').limit(1).execute()
+result = collection.find().execute()
 
 # Print document
-# docs = result.fetch_all()
-# print('Name: {0}'.format(docs[0]['name']))
+docs = result.fetch_all()
+print(docs)
 
 session.close()

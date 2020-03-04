f05a030743a19860d87187b05ba81ed909ac2ebe
setrar <setrar@valiha.com>
2020-03-03 19:03:36 -0500

    .

---
+++ 4.SQL-NoSQL/300098957/.pip/README.md
@@ -0,0 +1,3 @@
+# PIP Install
+
+https://dev.mysql.com/doc/dev/connector-python/8.0/installation.html#installing-connector-python-with-pip
---
+++ 4.SQL-NoSQL/300098957/.pip/b300098957.py
@@ -0,0 +1,23 @@
+import mysqlx
+
+# Connect to server on localhost
+session = mysqlx.get_session({
+    'host': 'localhost',
+    'port': 33060,
+    'user': 'etudiants',
+    'password': 'etudiants_1'
+})
+
+# schema = session.get_schema('test')
+
+# Use the collection 'my_collection'
+# collection = schema.get_collection('my_collection')
+
+# Specify which document to find with Collection.find()
+# result = collection.find('name like :param').bind('param', 'S%').limit(1).execute()
+
+# Print document
+# docs = result.fetch_all()
+# print('Name: {0}'.format(docs[0]['name']))
+
+session.close()
